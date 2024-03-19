8efff1b7ea82efdb4b4df5b29dfa11c2a80a9056
Durga-Grandhi <grandhi.usedu@gmail.com>
2024-03-18 23:40:55 -0500

    have written login api endpoints for consumer and provider and created listing shcema

---
+++ backend/src/models/listings.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    location: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    contactInfo: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    dietaryRestrictions: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    provider: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'provider',
+        required: true
+    },
+    consumer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'consumer',
+        required: true
+    },
+    useBy:{
+        type: Date,
+        required: true
+    
+    }
+});
+
+module.exports = mongoose.model('listing', listingSchema);
--- backend/src/routes/authentication.js
+++ backend/src/routes/authentication.js
@@ -28,3 +28,38 @@ router.post("/registration-consumer", async(req, res) => {
         return res.status(500).json({message: "Internal Server Error"});
     }
 });
+
+router.post("/login-provider", async(req, res) => {
+    try{
+        const {email, password} = req.body;
+        const provider = await providerModel.findOne({email});
+        if(!provider){
+            return res.status(400).json({message: "Provider not registered"});
+        }
+        if(provider.password !== password){
+            return res.status(400).json({message: "Invalid password"});
+        }
+        return res.status(200).json({message: "Provider logged in successfully",provider});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+})
+router.post("/login-consumer", async(req, res) => {
+    try{
+        const {email, password} = req.body;
+        const consumer = await consumerModel.findOne({email});
+        if(!consumer){
+            return res.status(400).json({message: "Consumer not registered"});
+        }
+        if(consumer.password !== password){
+            return res.status(400).json({message: "Invalid password"});
+        }
+        return res.status(200).json({message: "Consumer logged in successfully",consumer});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+})
+
+module.exports = router;
